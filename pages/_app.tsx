2a13cd9c70cfcaf578ebbc1b24388547bdf0618c
Shiva Reddy <readytohackk@gmail.com>
2022-03-05 17:10:10 +0530

    connect to dai using etherscan api

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,12 +7,27 @@ import { hasEthereum } from '../utils/ethereum';
 export const TokenContext = createContext({
   connectedWalletAddress: "",
   network: '',
-  setConnectedWalletAddress: () => { }
+  setConnectedWalletAddress: () => { },
+  daixBalance: '',
+  daiBalance: '',
+  ethBalance: ''
 });
+//todo move api to vercel/hosting platform
+const apiLink = `https://api-goerli.etherscan.io/api?action=getabi&apikey=IAIUXU71QTU8WNJWQBKTUSBJTVCPREWXU3`
+const tokenAddresses = [{
+  address: '0xF2d68898557cCb2Cf4C10c3Ef2B034b2a69DAD00',
+  token: 'DAIx'
+}, {
+  address: '0x88271d333C72e51516B67f5567c728E702b3eeE8',
+  token: 'DAI'
+}]
 function MyApp({ Component, pageProps }: AppProps) {
 
   const [connectedWalletAddress, setConnectedWalletAddressState] = useState("");
-  const [network, setNetwork] = useState('')
+  const [network, setNetwork] = useState('');
+  const [daixBalance, setDaixBalance] = useState('');
+  const [daiBalance, setDaiBalance] = useState('');
+  const [ethBalance, setEthBalance] = useState('');
   useEffect(() => {
     if (!hasEthereum()) {
       setConnectedWalletAddressState(``);
@@ -32,16 +47,69 @@ function MyApp({ Component, pageProps }: AppProps) {
       const network = await signer.provider.getNetwork();
       setNetwork(network.name)
       setConnectedWalletAddressState(`${signerAddress}`);
-    } catch {
+      // console.log(ethers.utils.formatEther(await signer.getGasPrice()), "test")
+      // console.log(ethers.utils.formatEther(await signer.estimateGas()), "test")
+      provider.getBalance(signerAddress).then((balance) => {
+        // convert a currency unit from wei to ether
+        const balanceInEth = ethers.utils.formatEther(balance)
+        setEthBalance(balanceInEth)
+      })
+
+      // const daixContract = new ethers.Contract(tokenAddresses[0].address, daixabi, provider);
+      // console.log(daixContract)
+      // daixContract.balanceOf(signerAddress).then((balance: string) => {
+      //   console.log(balance, "test")
+      //   setDaixBalance(balance)
+      // })
+      // console.log(await provider.listAccounts())
+      await getBalances()
+    } catch (e) {
+      console.log(e)
       return;
     }
   }
+
+  const getBalances = async () => {
+    const provider = new ethers.providers.Web3Provider((window as any).ethereum);
+    const signer = provider.getSigner();
+    try {
+      const signerAddress = await signer.getAddress();
+      const network = await signer.provider.getNetwork();
+      const networkName = network.name;
+      provider.getBalance(signerAddress).then((balance) => {
+        const balanceInEth = ethers.utils.formatEther(balance)
+        setEthBalance(balanceInEth)
+      })
+      const [daixABI, daiABI] = await Promise.all([
+        fetch(`${apiLink}&address=${tokenAddresses[0].address}&module=contract`).then(res => res.json()),
+        fetch(`${apiLink}&address=${tokenAddresses[1].address}&module=contract`).then(res => res.json()),
+      ])
+      console.log(daixABI, daiABI)
+      const daixContract = new ethers.Contract(tokenAddresses[0].address, daixABI.result, provider);
+      const daiContract = new ethers.Contract(tokenAddresses[1].address, daiABI.result, provider);
+      // daixContract.balanceOf(signerAddress).then((balance: string) => {
+      //   setDaixBalance(ethers.utils.formatEther(balance))
+      // })
+      daiContract.balanceOf(signerAddress).then((balance: string) => {
+        setDaiBalance(ethers.utils.formatEther(balance))
+      })
+    } catch (e) { console.log(e) }
+
+  }
+
+  // const daixContract = new ethers.Contract(tokenAddresses[0].address, daixabi, provider);
+  // console.log(daixContract)
+
+  // console.log(await provider.listAccounts())
 
 
   return <TokenContext.Provider value={{
     connectedWalletAddress: connectedWalletAddress,
     setConnectedWalletAddress: setConnectedWalletAddress,
-    network: network
+    network: network,
+    daixBalance: daixBalance,
+    daiBalance: daiBalance,
+    ethBalance: ethBalance
   }}> <Component {...pageProps} /></TokenContext.Provider>
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,91 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { TokenContext } from './_app';
 import RootLayout from '../layout/RootLayout';
+import { ethers } from 'ethers';
+import { Framework } from '@superfluid-finance/sdk-core';
 interface Props {
 
 }
-const Home = (props: Props) => {
 
+//where the Superfluid logic takes place
+async function createNewFlow(recipient: string, flowRate: string) {
+  const { ethereum } = window as any;
+  const provider = new ethers.providers.Web3Provider(ethereum);
+
+  const signer = await provider.getSigner();
+
+  const chainId = await ethereum.request({ method: "eth_chainId" });
+  const sf = await Framework.create({
+    chainId: Number(chainId),
+    provider: provider
+  });
+  console.log(Number(chainId), provider, flowRate, recipient);
+
+  const DAIx = "0xBF6201a6c48B56d8577eDD079b84716BB4918E8A";
+
+  try {
+    const createFlowOperation = sf.cfaV1.createFlow({
+      receiver: recipient,
+      flowRate: flowRate,
+      superToken: DAIx
+      // userData?: string
+    });
+
+    console.log("Creating your stream...");
+
+    const result = await createFlowOperation.exec(signer);
+    console.log(result);
+
+    console.log(
+      `Congrats - you've just created a money stream!
+    View Your Stream At: https://app.superfluid.finance/dashboard/${recipient}
+    Network: Kovan
+    Super Token: DAIx
+    Sender: 0x480C0B88C003e0fC3De3474343EC29EF95350dF8
+    Receiver: ${recipient},
+    FlowRate: ${flowRate}
+    `
+    );
+  } catch (error) {
+    console.log(error)
+    console.log(
+      "Hmmm, your transaction threw an error. Make sure that this stream does not already exist, and that you've entered a valid Ethereum address!"
+    );
+    console.error(error);
+  }
+}
+const Home = (props: Props) => {
+  const [recipient, setRecipient] = useState("");
+  const [isButtonLoading, setIsButtonLoading] = useState(false);
+  const [flowRate, setFlowRate] = useState("");
+  const [flowRateDisplay, setFlowRateDisplay] = useState("");
   const tokenContext = useContext(TokenContext);
+  function calculateFlowRate(amount: number) {
+    if (typeof Number(amount) !== "number" || isNaN(Number(amount)) === true) {
+      alert("You can only calculate a flowRate based on a number");
+      return;
+    } else if (typeof Number(amount) === "number") {
+      if (Number(amount) === 0) {
+        return 0;
+      }
+      const amountInWei = ethers.BigNumber.from(amount);
+      const monthlyAmount = ethers.utils.formatEther(amountInWei.toString());
+      const calculatedFlowRate = +monthlyAmount * 3600 * 24 * 30;
+      return calculatedFlowRate;
+    }
+  }
+  const handleRecipientChange = (e: any) => {
+    setRecipient(e.target.value);
+  };
+
+  const handleFlowRateChange = (e: any) => {
+    setFlowRate(e.target.value);
+    let newFlowRateDisplay = calculateFlowRate(e.target.value);
+    setFlowRateDisplay(newFlowRateDisplay!.toString());
+  };
+
   return (
     <RootLayout>
       <div className="flex min-h-screen flex-col items-center justify-center py-2">
@@ -42,7 +119,7 @@ const Home = (props: Props) => {
                   Dai Balance
                 </div>
                 <div className="text-slate-700 dark:text-slate-500 truncate">
-                  {tokenContext.connectedWalletAddress}
+                  {tokenContext.daiBalance}
                 </div>
               </div>
             </div>
@@ -52,16 +129,57 @@ const Home = (props: Props) => {
                   DaiX Balance
                 </div>
                 <div className="text-slate-700 dark:text-slate-500 truncate">
-                  {tokenContext.connectedWalletAddress}
+                  {tokenContext.daixBalance}
                 </div>
               </div>
             </div>
           </div>
+          <div className="bg-slate-300 rounded-xl p-8 dark:bg-slate-800 mt-5" >
+            <div className=" text-center space-y-4">
+              <div className="text-sky-500 dark:text-sky-400">
+                Approve and Deposit DAI into superfluid to ming DAIx
+              </div>
+              <div className="text-slate-700 dark:text-slate-500 flex justify-center">
+                <input placeholder='Amount' className='m-2'>
+                </input>
+              </div>
+              <div className='grid grid-cols-2 gap-x-5'>
+                <button className="w-full flex items-center justify-center px-8 py-2 border border-transparent text-base font-medium rounded-md text-white bg-green-600 hover:bg-green-700 md:py-2 md:text-lg md:px-10"
+                >
+                  Approve
+                </button>
+                <button className="w-full flex items-center justify-center px-8 py-2 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-2 md:text-lg md:px-10"
+                >
+                  Deposit
+                </button>
+              </div>
 
+            </div>
+          </div>
+          <div className="bg-slate-300 rounded-xl p-8 dark:bg-slate-800 mt-5" >
+            <div className=" text-center space-y-4">
+              <div className="text-sky-500 dark:text-sky-400">
+                Stream DAI
+              </div>
+              <div className="text-slate-700 dark:text-slate-500 flex">
+                <input placeholder='Enter Address to stream' className='m-2' onChange={handleRecipientChange}>
+                </input>
+                <input placeholder='DAIx/Month' className='m-2' onChange={handleFlowRateChange}>
+                </input>
+              </div>
+              <button className="w-full flex items-center justify-center px-8 py-2 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-2 md:text-lg md:px-10"
+                onClick={
+                  () => createNewFlow(recipient, flowRate)
+                }
+              >
+                Start Stream
+              </button>
+            </div>
+          </div>
         </main>
 
-      </div>
-    </RootLayout>
+      </div >
+    </RootLayout >
   )
 }
 
